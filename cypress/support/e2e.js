72cd870fcb3a2c9d8c19c9bbab56a121e28a1c76
dansocci <40389042+dansocci@users.noreply.github.com>
2024-08-22 15:06:19 +0100

    Delete cypress directory

--- cypress/support/e2e.js
+++
@@ -1,9 +0,0 @@
-import 'cypress-plugin-api'
-import 'cypress-mochawesome-reporter/register'
-import './api_commands_pt'
-import './api_error_responses_pt'
-import './api_commands_uk'
-import './api_error_responses_uk'
-
-const dayjs = require('dayjs')
-Cypress.dayjs = dayjs
